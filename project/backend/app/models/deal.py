b5af356ed9505380096d486509a39813963a31ec
saevelina56-tech <saevelina56@gmail.com>
2026-02-21 09:11:31 +0800

    chore(saevelina56-tech): регистрация студента

---
+++ project/backend/app/models/deal.py
@@ -0,0 +1,4 @@
+# from sqlalchemy import Column, Integer, String, Numeric, ForeignKey, DateTime
+# from sqlalchemy.orm import relationship
+from app.database import Base
+from datetime import datetime
